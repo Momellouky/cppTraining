a561c0dadac1c1cb23d2e3787482687c71d9c39d
Mohamed Mellouky <contact.mellouky@gmail.com>
2020-12-22 20:38:27 +0100

    Loops[For Loop]: right triangle

---
+++ cpp_training/Loops.cpp
@@ -0,0 +1,33 @@
+/*
+
+OUTPUT:
+
+*****
+ ****
+  ***
+   **
+    *
+
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 5; i >= 1; i--)
+    {
+        for (int j = 5; j >= 1; j--)
+        {
+            if (j <= i) cout << "*";
+            else cout << " ";
+
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
