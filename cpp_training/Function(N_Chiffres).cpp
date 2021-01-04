94c69e4ced79fc767a3702b1b1ec9a5e4413421d
Mohamed Mellouky <contact.mellouky@gmail.com>
2021-01-04 21:30:54 +0100

    fix[N_Chiffres fct]: handle n equal zero

--- cpp_training/Function(N_Chiffres).cpp
+++ cpp_training/Function(N_Chiffres).cpp
@@ -6,6 +6,14 @@
 * SO N_chiffres RETURNS 4
 *
 */
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <iostream>
 
 using namespace std; 
@@ -13,6 +21,8 @@ using namespace std;
 int N_chiffres(int n)
 {
     int counter = 0, Devision = n;
+    
+    if(Devision == 0) return 1; 
     
     while(Devision != 0)
     {
@@ -28,6 +38,6 @@ int N_chiffres(int n)
 int main()
 {
     
-    cout << N_chiffres(393) << endl; 
+    cout << N_chiffres(0) << endl; 
     return 0;
 }
