e02aaa269f60ada1c08ff06db0698b186157469e
Mohamed Mellouky <contact.mellouky@gmail.com>
2021-02-05 17:19:34 +0100

    Fix: front pointer && enqueue()

--- DS/queue_array_emplementation.cpp
+++ DS/queue_array_emplementation.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cassert>
+
+
 
 const int SIZE = 50;
 template <class type>
@@ -10,9 +13,9 @@ class queue
 public:
 	queue()
 	{
-		front = 0;
+		front = -1; 
 		rear  = -1;
-		count = 0;   
+		count = 0;  // empty queue.  
 	}
 
 	bool empty()
@@ -22,49 +25,66 @@ public:
 
 	bool full()
 	{
-		return count == SIZE -1; 
+		return count == SIZE - 1; 
 	}
 
 	void enQueue(type element)
 	{
 		if(full())
 			std::cout << "Queue is full!" << std::endl;
+		else if( count == 0) // if queue is empty. 
+		{
+			rear = 0;
+			front = 0; 
+			arr[rear] = element; 
+			count++;
+		}
 		else
 		{
-			rear = (rear + 1 ) % SIZE; 
+			rear = (rear + 1 ) % SIZE;
 			arr[rear] = element; 
+			count++;	
 		}
+
+		
+
 	}
 
-	void diQueue()
+	type deQueue()
 	{
 		if(empty())
 			std::cout << "Queue is empty!" << std::endl;
 		else
 		{
 			front = (front + 1) % SIZE;
+			count--;
 		}
+
+		return arr[--front]; 
 	}
 
 	type search(type element)
 	{
 
-		int position = -1, i = front; 
+		int position = -1, i = front, index = -1; 
 
 		for(i = front ; i != rear; i = (i + 1)% SIZE)
 		{
+
+			index++;
+
 			if(arr[i] == element)
 			{
-				position = i; 
+				position = index; 
 				break;  // return the first occurence
 			}
 		}
 
-		if(position == -1)
+		if(position == -1) // 
 		{
-			if(arr[i] == element)
+			if(arr[i] == element) // arr[i] <-- is the last element 
 				{
-					position = i; 
+					position = ++index; 
 				}
 		}
 
@@ -89,16 +109,19 @@ public:
 	int getFront()
 	{
 
-		assert(empty());
-		return front; 
-
+		if(!empty())
+			return arr[front]; 
+		else
+			return -1; 
 	}
 
 	int getRear()
 	{
 
-		assert(empty()); 
-		return rear;
+		if(!empty())
+			return arr[rear]; 
+		else
+			return -1;
 
 	}
 
@@ -111,9 +134,28 @@ int main()
 	queue<int> myq; 
 
 	myq.enQueue(20); 
-	myq.enQueue(50); 
+	myq.enQueue(50);
+	myq.enQueue(60);
+	myq.enQueue(100);
+
+	int front = myq.getFront(); 
+
+	std::cout << "front: " << front << std::endl; 
+
+	int rear = myq.getRear(); 
+
+	std::cout << "rear: " << rear << std::endl; 
 
 	myq.display(); 
+
+	myq.deQueue(); 
+	myq.deQueue();
+
+	myq.display(); 
+
+	int pos = myq.search(100);
+
+	std::cout << pos << std::endl; 
 
 	return 0;
 }
